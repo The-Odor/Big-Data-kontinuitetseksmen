9c19bfc7cf994c84388d45a8cc8574cbf0bdc503
The-Odor <theodor.midtbo.alstad@getmail.no>
2020-02-17 19:45:27 +0100

    Removed spaces in bash-scripts so they work
    done for tasks 2 and 4

--- Code/runBasis_windows.sh
+++ Code/runBasis_windows.sh
@@ -3,14 +3,14 @@
 # 2. the current directory contains files mapper.py and reducer.py for mapper and reducer code respectively
 
 #Simplify task change further
-taskNumber =
-taskName   =
-sourceFile =
+taskNumber=
+taskName=
+sourceFile=
 
 #Simplify task change
-mapperfile  = $taskNumber$taskName.py
-reducerfile = "$taskNumber"Reducer.py
-outfile     = output$taskNumber
+mapperfile= $taskNumber$taskName.py
+reducerfile= "$taskNumber"Reducer.py
+outfile= output$taskNumber
 
 #Automatic removal
 hadoop fs -rm -r $outfile
--- Code/task2/task2c/run2c_windows.sh
+++ Code/task2/task2c/run2c_windows.sh
@@ -3,14 +3,14 @@
 # 2. the current directory contains files mapper.py and reducer.py for mapper and reducer code respectively
 
 #Simplify task change further
-taskNumber = 2c
-taskName   = TopMiners
-sourceFile = Users
+taskNumber=2c
+taskName=TopMiners
+sourceFile=Users
 
 #Simplify task change
-mapperfile  = $taskNumber$taskName.py
-reducerfile = "$taskNumber"Reducer.py
-outfile     = output$taskNumber
+mapperfile=$taskNumber$taskName.py
+reducerfile="$taskNumber"Reducer.py
+outfile=output$taskNumber
 
 #Automatic removal
 hadoop fs -rm -r $outfile
